acadd0b3405a1f282924541f24c4c078925c0e71
Lea Foreman <leafuk@gmail.com>
2018-02-24 14:59:21 +0000

    Cache the Amazon profile so we don't query multiple times with the same token

--- src/modules/profile.js
+++ src/modules/profile.js
@@ -2,15 +2,27 @@
 var request = require('request');
 var amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
 
+var Storage = require('node-storage');
+var store = new Storage('/etc/homeautomation/data/state');
+
 exports.getProfile = function(accessToken, cb) {
     var url = amznProfileURL + encodeURIComponent(accessToken);
 
-    request.get(url, function(error, response, body) {
-        if (response.statusCode == 200) {
-            var profile = JSON.parse(body);
-            cb(null, profile);
-        } else {
-            cb(error);
-        }
-    });
+    var cachedProfile = store.get('profile-' + accessToken);
+    console.log('cached profile: ' + JSON.stringify(cachedProfile, null, 4));
+
+    if (cachedProfile) {
+        cb(null, cachedProfile);
+    } else {
+        request.get(url, function(error, response, body) {
+            if (response.statusCode == 200) {
+                var profile = JSON.parse(body);
+
+                store.put('profile-' + accessToken, profile);
+                cb(null, profile);
+            } else {
+                cb(error);
+            }
+        });
+    }
 };
